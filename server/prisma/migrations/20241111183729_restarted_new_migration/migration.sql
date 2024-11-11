a585493ab091e7fe66963c6907ffeeb2d8901304
Piyush Saini <134310734+piyushhsainii@users.noreply.github.com>
2024-11-12 00:08:30 +0530

    restarted migration

--- server/prisma/migrations/20241111183729_restarted_new_migration/migration.sql
+++ server/prisma/migrations/20241111183729_restarted_new_migration/migration.sql
@@ -1,6 +1,7 @@
 /*
   Warnings:
 
+  - A unique constraint covering the columns `[paymentId]` on the table `Payments` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `Tokens` to the `Payments` table without a default value. This is not possible if the table is not empty.
   - Added the required column `status` to the `Payments` table without a default value. This is not possible if the table is not empty.
 
@@ -8,3 +9,6 @@
 -- AlterTable
 ALTER TABLE "Payments" ADD COLUMN     "Tokens" TEXT NOT NULL,
 ADD COLUMN     "status" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Payments_paymentId_key" ON "Payments"("paymentId");
