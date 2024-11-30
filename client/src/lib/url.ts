5ad97d7b6a4690fce9bd411d47d4b57cee6bfe56
Piyush Saini <134310734+piyushhsainii@users.noreply.github.com>
2024-12-01 03:36:50 +0530

    fixed url

--- client/src/lib/url.ts
+++ client/src/lib/url.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://localhost:8000"
+// export const BACKEND_URL = "http://localhost:8000"
 // export const BACKEND_URL = "https://ideogram-model.onrender.com"
-// export const BACKEND_URL = "https://pixelbrew-ai-8sqzi.ondigitalocean.app"
+export const BACKEND_URL = "https://pixelbrew-ai-8sqzi.ondigitalocean.app"
